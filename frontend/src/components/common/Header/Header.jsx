f6acd52590cc09c1a0b55821607e4bb9d5679b65
IvyLeagueCoder <tranhainhatminh2004@gmail.com>
2025-12-30 07:44:47 +0700

    Them dia chi cua hang

--- frontend/src/components/common/Header/Header.jsx
+++ frontend/src/components/common/Header/Header.jsx
@@ -201,7 +201,9 @@ function Header() {
       {/* Icons & Giỏ hàng (Code theo logic mới của Team) */}
       <div className="flex items-center gap-5">
         <a
-          href="#"
+          href="https://maps.app.goo.gl/hi1koEvqrQyGp5j66"
+          target="_blank"
+          rel="noopener noreferrer"
           className="flex items-center gap-2 text-gray-800 font-medium text-sm hover:text-blue-500"
         >
           <FiMapPin />
